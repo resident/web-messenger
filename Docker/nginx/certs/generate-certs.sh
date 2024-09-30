868affa6b84de7ccac57c65e6cc7885a7daa57d0
resident <resident@safe.vg>
2024-09-30 13:20:27 +0300

    Add file .gitattributes

--- Docker/nginx/certs/generate-certs.sh
+++ Docker/nginx/certs/generate-certs.sh
@@ -4,7 +4,6 @@ crt="/etc/nginx/certs/server.crt"
 key="/etc/nginx/certs/server.key"
 
 if [[ ! -f $crt || ! -f $key ]]; then
-    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-     -keyout $key -out $crt \
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $key -out $crt \
      -subj "/C=US/ST=California/L=San Francisco/O=Web Messenger/OU=IT Department/CN=web-messenger.local"
 fi
