b1ddf72398865ab953352095f4ee04de408b0ccf
resident <resident@safe.vg>
2024-10-18 12:31:22 +0300

    Fix duration

--- src/resources/js/Pages/ChatRoom/Partials/RecordAudioMessage.jsx
+++ src/resources/js/Pages/ChatRoom/Partials/RecordAudioMessage.jsx
@@ -1,49 +1,62 @@
-import React, { useRef, useState, useEffect } from 'react'
-import { MicrophoneIcon, TrashIcon } from '@heroicons/react/24/solid'
+import React, {useEffect, useRef, useState} from 'react'
+import {MicrophoneIcon, TrashIcon} from '@heroicons/react/24/solid'
+import {fixWebmDuration} from "@fix-webm-duration/fix";
 
-export default function RecordAudioMessage({ selectedFiles, setSelectedFiles }) {
-  const [isRecording, setIsRecording] = useState(false);
-  const [mediaRecorder, setMediaRecorder] = useState(null);
-  const audioChunks = useRef([]);
+export default function RecordAudioMessage({selectedFiles, setSelectedFiles}) {
+    const [isRecording, setIsRecording] = useState(false);
+    const [mediaRecorder, setMediaRecorder] = useState(null);
+    const audioChunks = useRef([]);
+    const startTimeRef = useRef(null);
 
-  useEffect(() => {
-    if (isRecording) {
-      navigator.mediaDevices.getUserMedia({ audio: true })
-        .then(stream => {
-          const recorder = new MediaRecorder(stream);
-          setMediaRecorder(recorder);
+    useEffect(() => {
+        if (isRecording) {
+            navigator.mediaDevices.getUserMedia({audio: true})
+                .then(stream => {
+                    const mimeType = 'audio/webm';
+                    const extension = mimeType.split('/')[1];
+                    const recorder = new MediaRecorder(stream, {mimeType});
 
-          recorder.ondataavailable = event => {
-            audioChunks.current.push(event.data);
-          };
+                    setMediaRecorder(recorder);
 
-          recorder.onstop = () => {
-            const audioBlob = new Blob(audioChunks.current, { type: 'audio/ogg' });
-            const audioFile = new File([audioBlob], `voice_message_${Date.now()}`, { type: "audio/ogg" });
+                    recorder.ondataavailable = event => {
+                        audioChunks.current.push(event.data);
+                    };
 
-            audioChunks.current = [];
-            setSelectedFiles([audioFile]);
-          };
-          setSelectedFiles([]);
-          recorder.start();
-        })
-        .catch(error => {
-          console.error('Error accessing microphone:', error);
-        });
-    } else if (mediaRecorder && mediaRecorder.state === "recording") {
-      mediaRecorder.stop();
-    }
-  }, [isRecording]);
+                    recorder.onstop = async () => {
+                        const startTime = startTimeRef.current;
+                        const duration = Date.now() - startTime;
 
-  return (
-    <div>
-      <div
-        className={`flex gap-2 ${isRecording ? "bg-red-500" : "bg-gray-200 hover:bg-indigo-600 hover:text-white"} 
-                  p-3 rounded-full`} >
-        <MicrophoneIcon className="size-6 cursor-pointer" title='Record audio' onClick={() => setIsRecording(!isRecording)} />
-        <TrashIcon title='Click to delete' onClick={() => setSelectedFiles([])} className={`${selectedFiles.length > 0
-          ? 'size-6 cursor-pointer' : 'hidden'}`}>{selectedFiles.length}</TrashIcon>
-      </div>
-    </div>
-  );
+                        const audioBlob = new Blob(audioChunks.current, {type: mimeType});
+                        const audioBlobWithDuration = await fixWebmDuration(audioBlob, duration);
+                        const audioFile = new File([audioBlobWithDuration], `voice_message.${extension}`, {type: mimeType});
+
+                        audioChunks.current = [];
+                        setSelectedFiles([audioFile]);
+                    };
+
+                    setSelectedFiles([]);
+                    recorder.start();
+                    startTimeRef.current = Date.now();
+                })
+                .catch(error => {
+                    console.error('Error accessing microphone:', error);
+                });
+        } else if (mediaRecorder && mediaRecorder.state === "recording") {
+            mediaRecorder.stop();
+        }
+    }, [isRecording]);
+
+    return (
+        <div>
+            <div
+                className={`flex gap-2 ${isRecording ? "bg-red-500" : "bg-gray-200 hover:bg-indigo-600 hover:text-white"}
+                  p-3 rounded-full`}>
+                <MicrophoneIcon className="size-6 cursor-pointer" title='Record audio'
+                                onClick={() => setIsRecording(!isRecording)}/>
+                <TrashIcon title='Click to delete' onClick={() => setSelectedFiles([])}
+                           className={`${selectedFiles.length > 0
+                               ? 'size-6 cursor-pointer' : 'hidden'}`}>{selectedFiles.length}</TrashIcon>
+            </div>
+        </div>
+    );
 }
